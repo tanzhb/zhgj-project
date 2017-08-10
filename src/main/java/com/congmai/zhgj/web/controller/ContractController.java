cde580de749af31b892726710ab1a80e49fd953a
tanzb <63255661@qq.com>
2017-08-10 17:32:02 +0800

    修改冲突

--- src/main/java/com/congmai/zhgj/web/controller/ContractController.java
+++ src/main/java/com/congmai/zhgj/web/controller/ContractController.java
@@ -1,6 +1,5 @@
 package com.congmai.zhgj.web.controller;
 
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -15,16 +14,13 @@ import java.util.UUID;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.tomcat.util.http.parser.MediaType;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -36,7 +32,6 @@ import com.alibaba.druid.util.StringUtils;
 import com.congmai.zhgj.web.model.ContractVO;
 import com.congmai.zhgj.web.model.User;
 import com.congmai.zhgj.web.service.ContractService;
-import com.congmai.zhgj.web.service.UserService;
 
 
 /**
