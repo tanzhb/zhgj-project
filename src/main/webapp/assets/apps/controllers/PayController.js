398a53e7e06f7488f9470ca66d3a04232f5639e2
czw0020 <30466016+czw0020@users.noreply.github.com>
2017-10-17 09:44:38 +0800

    修改

--- src/main/webapp/assets/apps/controllers/PayController.js
+++ src/main/webapp/assets/apps/controllers/PayController.js
@@ -1209,7 +1209,7 @@ angular.module('MetronicApp').controller('PayController', ['$rootScope','$scope'
 					"sLast": "尾页"
 				}
 			},
-			order: [[1, "asc"]],// 默认排序列及排序方式
+			order: [[9, "desc"]],// 默认排序列及排序方式
 			searching: true,// 是否过滤检索
 			ordering:  true,// 是否排序
 			lengthMenu: [[5, 10, 15, 30, -1], [5, 10, 15, 30, "All"]],
